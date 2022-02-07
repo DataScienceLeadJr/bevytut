2cbc7139e63ba3a176bf9a0d6f7463e6458eaf3b
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2022-02-07 11:29:33 +0100

    refactor start part 2.

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
-#![allow(unused)]
+mod player;
 
-use bevy::{prelude::*, ecs::system::Command, asset::Asset};
+use bevy::prelude::*;
+use player::PlayerPlugin;
 
 const PLAYER_SPRITE: &str = "player_a_01.png";
 const LASER_SPRITE: &str = "laser_a_01.png";
@@ -13,6 +14,7 @@ pub struct Materials {
 }
 
 struct WinSize {
+    #[allow(unused)]
     width: f32,
     height: f32,
 }
@@ -41,10 +43,7 @@ fn main() {
         })
         .add_plugins(DefaultPlugins)
         .add_startup_system(setup.system())
-        .add_startup_stage("game_setup_actors", SystemStage::single(player_spawn.system()))
-        .add_system(player_movement.system())
-        .add_system(player_fire.system())
-        .add_system(laser_movement.system())
+        .add_plugin(PlayerPlugin)
         .run();
 }
 
@@ -54,7 +53,7 @@ fn setup(
     mut materials: ResMut<Assets<ColorMaterial>>,
     mut windows: ResMut<Windows>,
 ) {
-    let mut window = windows.get_primary_mut().unwrap();
+    let window = windows.get_primary_mut().unwrap();
     // camera
     commands.spawn_bundle(OrthographicCameraBundle::new_2d());
 
@@ -71,85 +70,4 @@ fn setup(
     // position window
     window.set_position(IVec2::new(2140,420));
 
-}
-
-fn player_spawn(
-    mut commands: Commands,
-    materials: Res<Materials>,
-    win_size: Res<WinSize>,
-) {
-    // spawn a sprite
-    let bottom = -win_size.height / 2.0;
-    commands
-        .spawn_bundle(SpriteBundle {
-            material: materials.player_materials.clone(),
-            transform: Transform {
-                translation: Vec3::new(0.0, bottom + 75.0 / 4.0 + 5.0, 10.0),
-                scale: Vec3::new(0.5, 0.5, 1.0),
-                ..Default::default()
-            },
-            ..Default::default()
-    })
-    .insert(Player)
-    .insert(PlayerReadyFire(true))
-    .insert(Speed::default());
-}
-
-fn player_movement(
-    keyboard_input: Res<Input<KeyCode>>,
-    mut query: Query<(&Speed, &mut Transform, With<Player>)>
-) {
-    if let Ok((speed, mut transform, _player)) = query.single_mut() {
-        let dir = if keyboard_input.pressed(KeyCode::Left) {
-            -1.0
-        } else if keyboard_input.pressed(KeyCode::Right) {
-            1.0
-        } else {
-            0.0
-        };
-        transform.translation.x += dir * speed.0 * TIME_STEP;
-    }
-}
-
-fn player_fire(
-    mut commands: Commands,
-    keyboard_input: Res<Input<KeyCode>>,
-    materials: Res<Materials>,
-    mut query: Query<(&Transform, &mut PlayerReadyFire, With<Player>)>
-) {
-    if let Ok((player_tf, mut ready_fire, _player)) = query.single_mut() {
-        if ready_fire.0 && keyboard_input.pressed(KeyCode::Space) {
-            let x = player_tf.translation.x;
-            let y = player_tf.translation.y + 15.0;
-            commands.spawn_bundle(SpriteBundle {
-                material: materials.laser.clone(),
-                transform: Transform {
-                    translation: Vec3::new(x, y, 0.0),
-                    ..Default::default()
-                },
-                ..Default::default()
-            })
-            .insert(Laser)
-            .insert(Speed::default());
-            ready_fire.0 = false;
-        }
-
-        if keyboard_input.just_released(KeyCode::Space) {
-            ready_fire.0 = true;
-        }
-    }
-}
-
-fn laser_movement(
-    mut commands: Commands,
-    win_size: Res<WinSize>,
-    mut query: Query<(Entity, &Speed, &mut Transform, With<Laser>)>
-) {
-    for (laser_entity, speed, mut laser_tf, _) in query.iter_mut() {
-        let translation = &mut laser_tf.translation;
-        translation.y += speed.0 * TIME_STEP;
-        if translation.y > win_size.height {
-            commands.entity(laser_entity).despawn();
-        }
-    }
 }
