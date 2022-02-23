aca41564a8d1c8b84f81542962f15c0696f5caaa
Sindre Fjermestad <37833410+DataScienceLeadJr@users.noreply.github.com>
2022-02-23 09:35:38 +0100

    wrap game init in arg match to be able to choose game.

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,22 @@
 mod invaders;
 
+use clap::{App, arg};
+
 use invaders::invaders_app;
 
 fn main() {
-    invaders_app();
+    let matches =  App::new("Bevy tutorials")
+                            .version("0.2")
+                            .author("Sindre Fjermestad")
+                            .about("Bevy engine tutorials followed")
+                            .arg(arg!(-g --game <GAME_NAME> "Specify which game tutorial to run.").required(true).ignore_case(true))
+                            .get_matches();
+    
+    match matches.value_of("game").unwrap() {
+        "invaders" => invaders_app(),
+        "gol" | "game-of-life" | "game_of_life"  => {
+            println!("GAME OF LIFE TIME!");
+        },
+        _ => panic!("WHAT WAS THAT!?"),
+    }
 }
